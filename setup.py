832a41cdaa65af3c8d2b133be0b93f441e434893
Proton <feisuzhu@163.com>
2017-10-23 19:22:04 +0800

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 
 try:
     from Cython.Distutils import build_ext
